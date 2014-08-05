33a1f58be6ca339b5b8c7e77b8a3f26fea8d3c48
Wen777 <shih777577@gmail.com>
2014-08-06 01:28:57 +0800

    it's a test. By meteor.js, we create a myGuesstbook.

---
+++ myGuesstbook.js
@@ -0,0 +1,33 @@
+﻿
+var postClient = new Meteor.Collection("myGuesstbook");
+
+if(Meteor.isClient) {
+    //Sent value to .html
+    Template.tem.helpers({
+        posts: postClient.find()
+    });
+    
+    //Client: If envet occured, do following operation.
+    Template.tem.events({
+        
+        //For submit icon being clicked...
+        "submit form": function(e){
+            //Default. Why?
+            e.preventDefault();
+            
+            //Sent data to Data Base. Also the value of id.
+            var postDB = {"text":$(e.target).find("[name=Cin]").val(),
+                          "author":$(e.target).find("[name=authorName]").val()};
+            postDB._id = postClient.insert(postDB);//why insert(postDB) ??
+            
+            
+            $(e.target).find("[name=Cin]").val("");
+        }
+    });
+}
+
+//Server
+
+if(Meteor.isServer) {
+    
+}
